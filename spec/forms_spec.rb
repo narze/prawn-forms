5798e4591983d1d88d9e25f90cb354a2c5781a2b
Nick Hammond <nick@nickhammond.com>
2012-01-20 13:52:57 -0600

    add tests to ensure the button method is adding the field to the Fields and page's Annots dictionary

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -17,4 +17,34 @@ describe "Prawn Forms" do
     FileUtils.rm("data/basic_pdf.pdf")
   end
   
+  describe "adding a button" do
+    it "sets the text properly" do
+      text = "MyButtonName"
+      
+      @pdf.should_receive(:add_interactive_field).with(
+        :Btn,
+        :T => Prawn::Core::LiteralString.new(text),
+        :DA => Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),
+        :F => 4,
+        :Ff => 65536,
+        :MK => {:CA => Prawn::Core::LiteralString.new(text), :BG => [0.75294, 0.75294, 0.75294], :BC => [0.75294, 0.75294, 0.75294]},
+        :Rect => [304.5, 537.39, 429, 552.39]
+      )
+
+      @pdf.button(text)
+    end
+
+    it "adds the button to the Fields dictionary" do
+      @pdf.button("ButtonName")
+      field = @pdf.state.store.root.data[:AcroForm].data[:Fields].select { |i| i.data[:T] == "ButtonName"}
+      field.size.should == 1
+    end
+    
+    it "adds the button to the page's Annots" do
+      @pdf.button("ButtonName")
+      field = @pdf.state.page.dictionary.data[:Annots].select { |i| i.data[:T] == "ButtodName"}
+      field.size.should == 1  
+    end
+  end
+  
 end
