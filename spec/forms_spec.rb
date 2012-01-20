e25675408ff062498334d9e3cb162ab891c1bdd5
Nick Hammond <nick@nickhammond.com>
2012-01-20 14:15:06 -0600

    add specs for adding a text field

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -18,33 +18,61 @@ describe "Prawn Forms" do
   end
   
   describe "adding a button" do
-    it "sets the text properly" do
-      text = "MyButtonName"
-      
-      @pdf.should_receive(:add_interactive_field).with(
-        :Btn,
-        :T => Prawn::Core::LiteralString.new(text),
+    before do
+      @options = {
+        :FT => :Btn,
+        :T => Prawn::Core::LiteralString.new("ButtonName"),
         :DA => Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),
         :F => 4,
         :Ff => 65536,
-        :MK => {:CA => Prawn::Core::LiteralString.new(text), :BG => [0.75294, 0.75294, 0.75294], :BC => [0.75294, 0.75294, 0.75294]},
-        :Rect => [304.5, 537.39, 429, 552.39]
-      )
-
-      @pdf.button(text)
-    end
-
-    it "adds the button to the Fields dictionary" do
-      @pdf.button("ButtonName")
-      field = @pdf.state.store.root.data[:AcroForm].data[:Fields].select { |i| i.data[:T] == "ButtonName"}
-      field.size.should == 1
+        :Type => :Annot,
+        :Subtype => :Widget,
+        :MK => {
+          :CA => Prawn::Core::LiteralString.new("ButtonName"), 
+          :BG => [0.75294, 0.75294, 0.75294], 
+          :BC => [0.75294, 0.75294, 0.75294]
+         },
+         :Rect => [304.5, 537.39, 429, 552.39]
+      }
     end
     
-    it "adds the button to the page's Annots" do
-      @pdf.button("ButtonName")
-      field = @pdf.state.page.dictionary.data[:Annots].select { |i| i.data[:T] == "ButtonName"}
-      field.size.should == 1  
+    it "adds the field to the Field's dictionary" do
+      @pdf.button(@options[:T])
+      
+      field = @pdf.state.store.root.data[:AcroForm].data[:Fields].select { |i| i.data[:T] == @options[:T]}.first
+      field.data.should == @options
     end
+    
+    it "references the same field in Fields & the current page's Annots" do
+      field = @pdf.state.store.root.data[:AcroForm].data[:Fields].select { |i| i.data[:T] == @options[:T]}.first
+      page_field = @pdf.state.page.dictionary.data[:Annots].select { |i| i.data[:T] == @options[:T]}.first      
+      
+      field.should == page_field
+    end
+    
   end
   
+  describe "adding a text field" do
+    before do
+      @options = {
+        :FT => :Tx,
+        :F => 4, 
+        :Rect => [36, 36, 136, -64], 
+        :Ff => 0, 
+        :MK => { :BC=> [0, 0, 0] }, 
+        :T => "TextFieldName", 
+        :BS => { :S => :S, :Type =>:Border, :W => 1}, 
+        :DA => "/Helv 0 Tf 0 g",
+        :Subtype => :Widget,
+        :Type => :Annot
+      }
+    end
+    
+    it "adds it to the Field's dictionary" do      
+      @pdf.text_field("TextFieldName", 0, 0, 100, 100)
+      
+      field = @pdf.state.store.root.data[:AcroForm].data[:Fields].select { |i| i.data[:T] == @options[:T]}.first
+      field.data.should == @options
+    end
+  end
 end
